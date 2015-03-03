895aa22ada08a04dd0121eec77a083090c43a498
marshalchen <cymcsg@gmail.com>
2015-03-03 15:14:09 +0800

    Add show and hide someview interface of UltimateRecyclerView

--- UltimateRecyclerView/ultimaterecyclerview/src/main/java/com/marshalchen/ultimaterecyclerview/UltimateRecyclerView.java
+++ UltimateRecyclerView/ultimaterecyclerview/src/main/java/com/marshalchen/ultimaterecyclerview/UltimateRecyclerView.java
@@ -5,13 +5,18 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.util.SparseIntArray;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.TranslateAnimation;
@@ -19,6 +24,8 @@ import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
 import com.marshalchen.ultimaterecyclerview.ui.DividerItemDecoration;
+import com.nineoldandroids.animation.ValueAnimator;
+import com.nineoldandroids.view.ViewHelper;
 
 /**
  * UltimateRecyclerView is a recyclerview which contains the function of swipe to dismiss,animations,drag drop etc.
@@ -39,6 +46,20 @@ public class UltimateRecyclerView extends FrameLayout {
     protected int mPaddingRight;
     protected boolean mClipToPadding;
     private UltimateViewAdapter mAdapter;
+    private ScrollState mScrollState;
+    private ScrollViewCallbacks mCallbacks;
+    private SparseIntArray mChildrenHeights = new SparseIntArray();
+    // Fields that should be saved onSaveInstanceState
+    private int mPrevFirstVisiblePosition;
+    private int mPrevFirstVisibleChildHeight = -1;
+    private int mPrevScrolledChildrenHeight;
+    private int mPrevScrollY;
+    private int mScrollY;
+
+
+    private boolean mFirstScroll;
+    private boolean mDragging;
+    private boolean mIntercepted;
 
     public UltimateRecyclerView(Context context) {
         super(context);
@@ -103,7 +124,7 @@ public class UltimateRecyclerView extends FrameLayout {
      * @param dismissCallbacks
      */
     public void setSwipeToDismissCallback(SwipeToDismissTouchListener.DismissCallbacks dismissCallbacks) {
-        int[] notToDismiss=null;
+        int[] notToDismiss = null;
         if (mAdapter.getCustomHeaderView() != null) {
             notToDismiss = new int[]{
                     0
@@ -173,6 +194,79 @@ public class UltimateRecyclerView extends FrameLayout {
                         staggeredGridLayoutManager.findLastVisibleItemPositions(lastPositions);
                         lastVisibleItemPosition = findMax(lastPositions);
                         break;
+                }
+                if (mCallbacks != null) {
+                    if (getChildCount() > 0) {
+                        int firstVisiblePosition = recyclerView.getChildPosition(recyclerView.getChildAt(0));
+                        int lastVisiblePosition = recyclerView.getChildPosition(recyclerView.getChildAt(getChildCount() - 1));
+                        for (int i = firstVisiblePosition, j = 0; i <= lastVisiblePosition; i++, j++) {
+                            if (mChildrenHeights.indexOfKey(i) < 0 || recyclerView.getChildAt(j).getHeight() != mChildrenHeights.get(i)) {
+                                mChildrenHeights.put(i, recyclerView.getChildAt(j).getHeight());
+                            }
+                        }
+
+                        View firstVisibleChild = recyclerView.getChildAt(0);
+                        if (firstVisibleChild != null) {
+                            if (mPrevFirstVisiblePosition < firstVisiblePosition) {
+                                // scroll down
+                                int skippedChildrenHeight = 0;
+                                if (firstVisiblePosition - mPrevFirstVisiblePosition != 1) {
+                                    for (int i = firstVisiblePosition - 1; i > mPrevFirstVisiblePosition; i--) {
+                                        if (0 < mChildrenHeights.indexOfKey(i)) {
+                                            skippedChildrenHeight += mChildrenHeights.get(i);
+                                        } else {
+                                            // Approximate each item's height to the first visible child.
+                                            // It may be incorrect, but without this, scrollY will be broken
+                                            // when scrolling from the bottom.
+                                            skippedChildrenHeight += firstVisibleChild.getHeight();
+                                        }
+                                    }
+                                }
+                                mPrevScrolledChildrenHeight += mPrevFirstVisibleChildHeight + skippedChildrenHeight;
+                                mPrevFirstVisibleChildHeight = firstVisibleChild.getHeight();
+                            } else if (firstVisiblePosition < mPrevFirstVisiblePosition) {
+                                // scroll up
+                                int skippedChildrenHeight = 0;
+                                if (mPrevFirstVisiblePosition - firstVisiblePosition != 1) {
+                                    for (int i = mPrevFirstVisiblePosition - 1; i > firstVisiblePosition; i--) {
+                                        if (0 < mChildrenHeights.indexOfKey(i)) {
+                                            skippedChildrenHeight += mChildrenHeights.get(i);
+                                        } else {
+                                            // Approximate each item's height to the first visible child.
+                                            // It may be incorrect, but without this, scrollY will be broken
+                                            // when scrolling from the bottom.
+                                            skippedChildrenHeight += firstVisibleChild.getHeight();
+                                        }
+                                    }
+                                }
+                                mPrevScrolledChildrenHeight -= firstVisibleChild.getHeight() + skippedChildrenHeight;
+                                mPrevFirstVisibleChildHeight = firstVisibleChild.getHeight();
+                            } else if (firstVisiblePosition == 0) {
+                                mPrevFirstVisibleChildHeight = firstVisibleChild.getHeight();
+                            }
+                            if (mPrevFirstVisibleChildHeight < 0) {
+                                mPrevFirstVisibleChildHeight = 0;
+                            }
+                            mScrollY = mPrevScrolledChildrenHeight - firstVisibleChild.getTop();
+                            mPrevFirstVisiblePosition = firstVisiblePosition;
+
+                            mCallbacks.onScrollChanged(mScrollY, mFirstScroll, mDragging);
+                            if (mFirstScroll) {
+                                mFirstScroll = false;
+                            }
+
+                            if (mPrevScrollY < mScrollY) {
+                                //down
+                                mScrollState = ScrollState.UP;
+                            } else if (mScrollY < mPrevScrollY) {
+                                //up
+                                mScrollState = ScrollState.DOWN;
+                            } else {
+                                mScrollState = ScrollState.STOP;
+                            }
+                            mPrevScrollY = mScrollY;
+                        }
+                    }
                 }
             }
 
@@ -502,5 +596,194 @@ public class UltimateRecyclerView extends FrameLayout {
             invalidate();
         }
     }
+
+    public void setScrollViewCallbacks(ScrollViewCallbacks listener) {
+        mCallbacks = listener;
+    }
+
+    @Override
+    public void onRestoreInstanceState(Parcelable state) {
+        SavedState ss = (SavedState) state;
+        mPrevFirstVisiblePosition = ss.prevFirstVisiblePosition;
+        mPrevFirstVisibleChildHeight = ss.prevFirstVisibleChildHeight;
+        mPrevScrolledChildrenHeight = ss.prevScrolledChildrenHeight;
+        mPrevScrollY = ss.prevScrollY;
+        mScrollY = ss.scrollY;
+        mChildrenHeights = ss.childrenHeights;
+        super.onRestoreInstanceState(ss.getSuperState());
+    }
+
+    @Override
+    public Parcelable onSaveInstanceState() {
+        Parcelable superState = super.onSaveInstanceState();
+        SavedState ss = new SavedState(superState);
+        ss.prevFirstVisiblePosition = mPrevFirstVisiblePosition;
+        ss.prevFirstVisibleChildHeight = mPrevFirstVisibleChildHeight;
+        ss.prevScrolledChildrenHeight = mPrevScrolledChildrenHeight;
+        ss.prevScrollY = mPrevScrollY;
+        ss.scrollY = mScrollY;
+        ss.childrenHeights = mChildrenHeights;
+        return ss;
+    }
+
+    /**
+     * This saved state class is a Parcelable and should not extend
+     * {@link android.view.View.BaseSavedState} nor {@link android.view.AbsSavedState}
+     * because its super class AbsSavedState's constructor
+     * {@link android.view.AbsSavedState#AbsSavedState(android.os.Parcel)} currently passes null
+     * as a class loader to read its superstate from Parcelable.
+     * This causes {@link android.os.BadParcelableException} when restoring saved states.
+     * <p/>
+     * The super class "RecyclerView" is a part of the support library,
+     * and restoring its saved state requires the class loader that loaded the RecyclerView.
+     * It seems that the class loader is not required when restoring from RecyclerView itself,
+     * but it is required when restoring from RecyclerView's subclasses.
+     */
+    static class SavedState implements Parcelable {
+        public static final SavedState EMPTY_STATE = new SavedState() {
+        };
+
+        int prevFirstVisiblePosition;
+        int prevFirstVisibleChildHeight = -1;
+        int prevScrolledChildrenHeight;
+        int prevScrollY;
+        int scrollY;
+        SparseIntArray childrenHeights;
+
+        // This keeps the parent(RecyclerView)'s state
+        Parcelable superState;
+
+        /**
+         * Called by EMPTY_STATE instantiation.
+         */
+        private SavedState() {
+            superState = null;
+        }
+
+        /**
+         * Called by onSaveInstanceState.
+         */
+        SavedState(Parcelable superState) {
+            this.superState = superState != EMPTY_STATE ? superState : null;
+        }
+
+        /**
+         * Called by CREATOR.
+         */
+        private SavedState(Parcel in) {
+            // Parcel 'in' has its parent(RecyclerView)'s saved state.
+            // To restore it, class loader that loaded RecyclerView is required.
+            Parcelable superState = in.readParcelable(RecyclerView.class.getClassLoader());
+            this.superState = superState != null ? superState : EMPTY_STATE;
+
+            prevFirstVisiblePosition = in.readInt();
+            prevFirstVisibleChildHeight = in.readInt();
+            prevScrolledChildrenHeight = in.readInt();
+            prevScrollY = in.readInt();
+            scrollY = in.readInt();
+            childrenHeights = new SparseIntArray();
+            final int numOfChildren = in.readInt();
+            if (0 < numOfChildren) {
+                for (int i = 0; i < numOfChildren; i++) {
+                    final int key = in.readInt();
+                    final int value = in.readInt();
+                    childrenHeights.put(key, value);
+                }
+            }
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            out.writeParcelable(superState, flags);
+
+            out.writeInt(prevFirstVisiblePosition);
+            out.writeInt(prevFirstVisibleChildHeight);
+            out.writeInt(prevScrolledChildrenHeight);
+            out.writeInt(prevScrollY);
+            out.writeInt(scrollY);
+            final int numOfChildren = childrenHeights == null ? 0 : childrenHeights.size();
+            out.writeInt(numOfChildren);
+            if (0 < numOfChildren) {
+                for (int i = 0; i < numOfChildren; i++) {
+                    out.writeInt(childrenHeights.keyAt(i));
+                    out.writeInt(childrenHeights.valueAt(i));
+                }
+            }
+        }
+
+        public Parcelable getSuperState() {
+            return superState;
+        }
+
+        public static final Parcelable.Creator<SavedState> CREATOR
+                = new Parcelable.Creator<SavedState>() {
+            @Override
+            public SavedState createFromParcel(Parcel in) {
+                return new SavedState(in);
+            }
+
+            @Override
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+        if (mCallbacks != null) {
+            switch (ev.getActionMasked()) {
+                case MotionEvent.ACTION_DOWN:
+                    // Whether or not motion events are consumed by children,
+                    // flag initializations which are related to ACTION_DOWN events should be executed.
+                    // Because if the ACTION_DOWN is consumed by children and only ACTION_MOVEs are
+                    // passed to parent (this view), the flags will be invalid.
+                    // Also, applications might implement initialization codes to onDownMotionEvent,
+                    // so call it here.
+                    mFirstScroll = mDragging = true;
+                    mCallbacks.onDownMotionEvent();
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    mIntercepted = false;
+                    mDragging = false;
+                    mCallbacks.onUpOrCancelMotionEvent(mScrollState);
+                    break;
+            }
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        Logs.d("mCallbacks   " + (mCallbacks == null));
+        if (mCallbacks != null) {
+            switch (ev.getActionMasked()) {
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    mIntercepted = false;
+                    mDragging = false;
+                    mCallbacks.onUpOrCancelMotionEvent(mScrollState);
+                    break;
+            }
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    public boolean toolbarIsShown(Toolbar mToolbar) {
+        return ViewHelper.getTranslationY(mToolbar) == 0;
+    }
+
+    public boolean toolbarIsHidden(Toolbar mToolbar) {
+        return ViewHelper.getTranslationY(mToolbar) == -mToolbar.getHeight();
+    }
+
+
 
 }
