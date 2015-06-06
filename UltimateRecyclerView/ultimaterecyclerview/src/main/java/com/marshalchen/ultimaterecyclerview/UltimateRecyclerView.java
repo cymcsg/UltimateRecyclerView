f65d971c3fe10f4e513fcc933d762d810b509c95
marshalchen <cymcsg@gmail.com>
2015-06-06 11:53:50 +0800

    update some description of UltimateRecyclerview

--- UltimateRecyclerView/ultimaterecyclerview/src/main/java/com/marshalchen/ultimaterecyclerview/UltimateRecyclerView.java
+++ UltimateRecyclerView/ultimaterecyclerview/src/main/java/com/marshalchen/ultimaterecyclerview/UltimateRecyclerView.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright(c) 2015 Marshal Chen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.marshalchen.ultimaterecyclerview;
 
 import android.content.Context;
@@ -32,7 +48,7 @@ import com.nineoldandroids.animation.ValueAnimator;
 import com.nineoldandroids.view.ViewHelper;
 
 /**
- * UltimateRecyclerView is a recyclerview which contains the function of swipe to dismiss,animations,drag drop etc.
+ * UltimateRecyclerView is a recyclerview which contains many features like  swipe to dismiss,animations,drag drop etc.
  */
 public class UltimateRecyclerView extends FrameLayout {
     public RecyclerView mRecyclerView;
@@ -192,22 +208,7 @@ public class UltimateRecyclerView extends FrameLayout {
         }
     }
 
-//    /**
-//     * Set a swipe-to-dismiss OnItemTouchListener for RecyclerView
-//     *
-//     * @param dismissCallbacks
-//     */
-//    public void setSwipeToDismissCallback(SwipeToDismissTouchListener.DismissCallbacks dismissCallbacks) throws NullPointerException {
-//        int[] notToDismiss = null;
-//        if (mAdapter == null) throw new NullPointerException();
-//        if (mAdapter != null && mAdapter.getCustomHeaderView() != null) {
-//            notToDismiss = new int[]{
-//                    0
-//            };
-//        }
-//
-//        mRecyclerView.addOnItemTouchListener(new SwipeToDismissTouchListener(mRecyclerView, dismissCallbacks, notToDismiss));
-//    }
+
 
     void setDefaultScrollListener() {
         mOnScrollListener = new RecyclerView.OnScrollListener() {
