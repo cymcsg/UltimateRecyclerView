a9c88fa8fe277dab3e4df78d1f1d017e56060563
世外桃源 <jobhesk@gmail.com>
2016-02-16 18:03:25 +0800

    before final fix

--- UltimateRecyclerView/app/src/main/java/com/marshalchen/ultimaterecyclerview/demo/loadmoredemo/DebugLoadMoreActivity.java
+++ UltimateRecyclerView/app/src/main/java/com/marshalchen/ultimaterecyclerview/demo/loadmoredemo/DebugLoadMoreActivity.java
@@ -255,7 +255,7 @@ public class DebugLoadMoreActivity extends AppCompatActivity {
         findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                simpleRecyclerViewAdapter.insertLast("rand added item");
+                simpleRecyclerViewAdapter.insertFirst("rand added item");
                 // - simpleRecyclerViewAdapter.in
             }
         });
@@ -263,8 +263,7 @@ public class DebugLoadMoreActivity extends AppCompatActivity {
         findViewById(R.id.del).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                simpleRecyclerViewAdapter.removeAt(1);
-                // - simpleRecyclerViewAdapter.in
+                simpleRecyclerViewAdapter.removeLast();
             }
         });
 
@@ -285,7 +284,7 @@ public class DebugLoadMoreActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_loadmore);
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
